626b6eb5003c46fd0b724dd73c31ca626e1a1f5b
ogenisoglu <omer.genisoglu@gmail.com>
2022-04-08 12:57:34 +0300

    sa

--- ticaret/routes/web.php
+++ ticaret/routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('giris');
+Route::get('/',function(){
+   echo 'null';
+});
+Route::get('/{name}', function ($name = null) {
+    echo $name." yazdırıldı";
 });
